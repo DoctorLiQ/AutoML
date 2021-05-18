1e6536894bc36f1200070a84e14e8ece79f8591c
nk0307@sina.com <nk0307@sina.com>
2021-05-18 16:19:28 +0800

    update

--- cnn/train_imagenet.py
+++ cnn/train_imagenet.py
@@ -209,11 +209,11 @@ def main():
         logging.info("Training Start at %d"%start_epoch)
         sat= torch.load(ckpt_file)
         # print(type(sat))
-        if num_gpus > 1:
-            model.load_state_dict(sat)
-        else:
-            model = nn.DataParallel().cuda()
-        model.load_state_dict(sat)
+        # if num_gpus > 1:
+        #     model.load_state_dict(sat)
+        # else:
+        #     model = nn.DataParallel().cuda()
+        model.module.load_state_dict(sat)
 
 
 #    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.decay_period, gamma=args.gamma, last_epoch=start_epoch)
